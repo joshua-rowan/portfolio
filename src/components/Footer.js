cb4e1b3e5cfc58de3feca01c3343e1ecc94aeb81
CardsClaw <joshr1979@gmail.com>
2023-09-12 14:12:40 -0500

    3 links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-      Here will be the footer.
+      <ul>
+        <li><a href="https://github.com/joshua-rowan">My GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/joshua-rowan-developer/">My LinkedIn</a></li>
+        <li><a href="https://www.youtube.com/channel/UCYNyVfxP8uEvC4k1wlwBY9w">My YouTube</a></li>
+      </ul>
     </footer>
   );
 }
