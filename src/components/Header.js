8e1359300b7123def05230bc9833fef4bf2495e2
CardsClaw <joshr1979@gmail.com>
2023-09-12 14:50:54 -0500

    Completed About Me Text and updated Header with my name and deleted test phrase

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header>
-      <h1>This is the beginning of my header. It will probably by my name.</h1>
-      <p>
-        Does this update to GH Pages automatically?
-      </p>
+      <h1>Joshua Rowan</h1>
     </header>
   );
 }
